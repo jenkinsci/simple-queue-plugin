b975d1fa103aa1cf23ebd1d459d677b1940266fd
Vincent Latombe <vincent@latombe.net>
2023-09-11 09:47:59 +0200

    [JENKINS-71884] Fix simple queue setup for 2.414.1+

--- src/main/java/cz/mendelu/xotradov/SimpleQueueWidget.java
+++ src/main/java/cz/mendelu/xotradov/SimpleQueueWidget.java
@@ -1,29 +1,52 @@
 package cz.mendelu.xotradov;
 
+import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.Extension;
 import hudson.ExtensionComponent;
+import hudson.model.View;
 import hudson.widgets.Widget;
-
-import java.util.logging.Logger;
+import java.util.Collection;
+import java.util.List;
 import jenkins.ExtensionFilter;
 import jenkins.widgets.BuildQueueWidget;
+import jenkins.widgets.WidgetFactory;
+
 
 /**
  * SimpleQueueWidget replaces the default BuildQueueWidget and adds arrows to each buildable item.
  */
-@SuppressWarnings("unused") //justification: used because of the @Extension below
-@Extension(ordinal=199)
 public class SimpleQueueWidget extends Widget {
-    private final static Logger logger = Logger.getLogger(SimpleQueueWidget.class.getName());
+    @SuppressWarnings("unused") // jelly
     public static String getMoveTypeName(){return MoveAction.MOVE_TYPE_PARAM_NAME;}
+    @SuppressWarnings("unused") // jelly
     public static String getItemIdName(){return MoveAction.ITEM_ID_PARAM_NAME;}
+    @SuppressWarnings("unused") // jelly
     public static String getViewNameParamName(){return MoveAction.VIEW_NAME_PARAM_NAME;}
+
+    @Extension(ordinal = 100)
+    public static final class ViewFactoryImpl extends WidgetFactory<View, SimpleQueueWidget> {
+        @Override
+        public Class<View> type() {
+            return View.class;
+        }
+
+        @Override
+        public Class<SimpleQueueWidget> widgetType() {
+            return SimpleQueueWidget.class;
+        }
+
+        @NonNull
+        @Override
+        public Collection<SimpleQueueWidget> createFor(@NonNull View target) {
+            return List.of(new SimpleQueueWidget());
+        }
+    }
 
     @Extension
     public static final class RemoveDefaultQueueWidget extends ExtensionFilter {
         @Override
         public <T> boolean allows(Class<T> type, ExtensionComponent<T> component) {
-            return Widget.class != type || !BuildQueueWidget.class.isInstance(component.getInstance());
+            return WidgetFactory.class != type || !BuildQueueWidget.ViewFactoryImpl.class.isInstance(component.getInstance());
         }
     }
 }

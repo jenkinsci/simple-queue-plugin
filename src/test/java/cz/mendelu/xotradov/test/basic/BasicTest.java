0efec5f9b3271c23b4057253a7fb55ed73e56e7a
Vincent Latombe <vincent@latombe.net>
2023-09-18 09:11:41 +0200

    Fix BasicTest

--- src/test/java/cz/mendelu/xotradov/test/basic/BasicTest.java
+++ src/test/java/cz/mendelu/xotradov/test/basic/BasicTest.java
@@ -1,31 +1,17 @@
 package cz.mendelu.xotradov.test.basic;
 
+import static org.junit.Assert.assertTrue;
+
 import cz.mendelu.xotradov.SimpleQueueWidget;
-import cz.mendelu.xotradov.test.TestHelper;
-import hudson.widgets.Widget;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 
-import java.util.logging.Logger;
-
-import static org.junit.Assert.*;
-
 public class BasicTest {
-    public static Logger logger = Logger.getLogger(BasicTest.class.getName());
     @Rule
     public JenkinsRule jenkinsRule = new JenkinsRule();
-    private TestHelper helper = new TestHelper(jenkinsRule);
-
     @Test
-    public void widgetPresenceTest(){
-        boolean presence = false;
-        for (Widget widget1: jenkinsRule.jenkins.getWidgets()){
-            if (widget1 instanceof SimpleQueueWidget) {
-                presence = true;
-                break;
-            }
-        }
-        assertTrue(presence);
+    public void widgetPresenceTest() {
+        assertTrue(jenkinsRule.jenkins.getPrimaryView().getWidgets().stream().anyMatch(SimpleQueueWidget.class::isInstance));
     }
-    }
+}

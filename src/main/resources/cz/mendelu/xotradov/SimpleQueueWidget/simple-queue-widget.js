e076a3586cc0352b593a1b8af4d00459c9a8d30b
Markus Winter <m.winter@sap.com>
2026-03-08 00:38:59 +0100

    [JENKINS-64705] update queue without form submission
    Submitting a form when clicking the links to change the queue order is
    expensive and makes changing several queue entries shortyl after each
    other.
    This change adjusts this by performing a post request within javascript
    to the `doMove` endpoint and then reloads only the widget.

---
+++ src/main/resources/cz/mendelu/xotradov/SimpleQueueWidget/simple-queue-widget.js
@@ -0,0 +1,28 @@
+Behaviour.specify(".simple-move-link", "simple-queue-widget", 0, function (element) {
+    const dataHolder = document.getElementById("simple-queue-data-holder");
+    const url = dataHolder.dataset.moveUrl;
+    const viewOwner = dataHolder.dataset.viewOwner;
+    const viewName = dataHolder.dataset.viewName;
+    element.addEventListener("click", function (event) {
+        event.preventDefault();
+        const params = new URLSearchParams({
+            itemId: element.dataset.itemId,
+            viewOwner: viewOwner,
+            viewName: viewName,
+            moveType: element.dataset.moveType
+        });
+        fetch(url + "?" + params, {
+            method: "POST",
+            cache: "no-cache",
+            headers: crumb.wrap({
+                "Content-Type": "application/x-www-form-urlencoded",
+            })
+        }).then((response) => {
+            if (response.ok) {
+                refreshPart(dataHolder.dataset.id, dataHolder.dataset.url);
+            } else {
+                console.warn(`SimpleQueueWidget move request failed with status ${response.status}`);
+            }
+        }).catch((error) => console.warn(`SimpleQueueWidget move request failed: ${error}`));
+    });
+});

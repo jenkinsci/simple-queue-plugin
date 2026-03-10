64766919f174120de77de50004a9bddf1efcb186
Jim Klimov <jimklimov+jenkinsci@gmail.com>
2026-03-10 15:10:53 +0100

    SimpleQueueWidget/index.jelly, bulk-move-buttons.js: fix externalized JS fixup of bulk move buttons onclick action
    Signed-off-by: Jim Klimov <jimklimov+jenkinsci@gmail.com>

--- src/main/resources/cz/mendelu/xotradov/SimpleQueueWidget/bulk-move-buttons.js
+++ src/main/resources/cz/mendelu/xotradov/SimpleQueueWidget/bulk-move-buttons.js
@@ -1,22 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // OnClick code to POST an HTTP request inspired by validateButton() at
-    // https://github.com/jenkinsci/jenkins/blob/838cafb8fb86fd843b85d2e22e957e09a1ce86a9/war/src/main/webapp/scripts/hudson-behavior.js#L2581
+// OnClick code to POST an HTTP request inspired by validateButton() at
+// https://github.com/jenkinsci/jenkins/blob/838cafb8fb86fd843b85d2e22e957e09a1ce86a9/war/src/main/webapp/scripts/hudson-behavior.js#L2581
+document.querySelectorAll('.BulkMoveRegex_button').forEach(button => {
+    button.onclick = function() {
+        var parameters = {};
+        // ex-jelly: '${it.getMoveTypeName()}'
+        parameters['moveType']=button.dataset.var;   // (named via "data-var" in original caller)
+        // ex-jelly: '${it.getItemIdName()}'
+        parameters['itemId']=findPreviousFormItem(this, 'BulkMoveRegex').value;
 
-    document.querySelectorAll('BulkMoveRegex_button_up_fast').forEach(button => {
-        button.onclick = function() {
-            var parameters = {};
-            parameters['${it.getMoveTypeName()}']='UP_FAST';
-            parameters['${it.getItemIdName()}']=findPreviousFormItem(this, 'BulkMoveRegex').value;
-            fetch('${rootURL}/simpleMove/move', { method: 'post', body: objectToUrlFormEncoded(parameters), headers: crumb.wrap({'Content-Type': 'application/x-www-form-urlencoded',})})
-        }
-    })
+        // Tried to use jenkins.baseUrl provided by jenkins.js but it is not global or something?
+        fetch(document.head.dataset.rooturl + '/simpleMove/move', {
+            method: 'post',
+            body: objectToUrlFormEncoded(parameters),
+            headers: crumb.wrap({'Content-Type': 'application/x-www-form-urlencoded',})
+        })
+    }
+})
 
-    document.querySelectorAll('BulkMoveRegex_button_down_fast').forEach(button => {
-        button.onclick = function() {
-            var parameters = {};
-            parameters['${it.getMoveTypeName()}']='DOWN_FAST';
-            parameters['${it.getItemIdName()}']=findPreviousFormItem(this, 'BulkMoveRegex').value;
-            fetch('${rootURL}/simpleMove/move', { method: 'post', body: objectToUrlFormEncoded(parameters), headers: crumb.wrap({'Content-Type': 'application/x-www-form-urlencoded',})})
-        }
-    })
-}
